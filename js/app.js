84c7531e6ac8b8871d19eb09d31625a09c384fb7
vicnet <jonanv@uw.edu>
2014-10-13 12:13:57 -0700

    jsjsjsjjs

---
+++ js/app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var name = 'Dave';
+var fruits = ['apple', 'pear', 'kiwi', 'banana' ];
+//alert('Hello ' + name);
+var animalSound = document.getElementById('animalsound');
+
+document.addEventListener('DOMContentLoaded', function() {
+    var msgElem = document.getElementById('message');
+    msgElem.innerHTML = 'Hello ' + name;
+    var fruitLi;
+
+    var i;
+    var fruitListE = document.getElementById('fruit-list');
+    for (i = 0; i < fruits.length; ++i) {
+        console.log(fruits[i]);
+
+        fruitLi = document.createElement('li');
+        fruitLi.innerHTML = fruits[i];
+        fruitListE.appendChild(fruitLi);
+
+//        msgElem.innerHTML = fruits[i] + ', ' + msgElem.innerHTML;
+    }
+
+    var animalPic = document.getElementById('animal-picture');
+    animalPic.addEventListener('click', function () {
+        msgElem.innerHTML = 'woof!';
+
+        while(true) {
+            animalSound.play();
+        }
+
+        for (var idx = 0; idx < 11; idx++) {
+            alert('');
+
+        }
+
+    });
+
+    window.setInterval(function () {
+        document.getElementById('clock').innerHTML = new Date().toLocaleString();
+
+    }, 1000);
+});
+
+
